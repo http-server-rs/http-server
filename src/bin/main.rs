11a82d71ac136ba87edfd30c925aae322903b263
Esteban Borai <estebanborai@gmail.com>
2021-06-22 23:16:28 -0400

    feat(bench): setup dehat profiling and benchmarks

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,8 +1,18 @@
 use http_server_lib::make_server;
 use std::process::exit;
 
+#[cfg(feature = "dhat-profiling")]
+use dhat::{Dhat, DhatAlloc};
+
+#[cfg(feature = "dhat-profiling")]
+#[global_allocator]
+static ALLOCATOR: DhatAlloc = DhatAlloc;
+
 #[tokio::main]
 async fn main() {
+    #[cfg(feature = "dhat-profiling")]
+    let _dhat = Dhat::start_heap_profiling();
+
     match make_server() {
         Ok(server) => {
             server.run().await;
