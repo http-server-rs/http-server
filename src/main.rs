b85c24852e5b3cb5d7a11332a6e3677d745489a6
Phosphorus <fernando.n.p.d.moscu@gmail.com>
2023-05-14 22:40:17 -0300

    feat: add color-eyre as a dependecy

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,15 @@ mod extension;
 mod server;
 
 use clap::Parser;
+use color_eyre::eyre::Result;
 
 use self::cli::Cli;
 use self::server::Server;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
+    color_eyre::install()?;
+
     let cli = Cli::parse();
     let address = cli.address();
     let server = Server::from(cli);
@@ -19,4 +22,5 @@ async fn main() {
         .serve(server.router().into_make_service())
         .await
         .unwrap();
+    Ok(())
 }
