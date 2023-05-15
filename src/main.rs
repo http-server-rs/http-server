ad920b9ad17a20a9c2f2353d835f6aa2ff75a785
Esteban Borai <estebanborai@gmail.com>
2023-05-14 20:48:03 -0400

    feat: provide axum http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use axum::{routing::get, Router};
 use clap::Parser;
 
 #[derive(Debug, Parser)]
@@ -11,7 +12,14 @@ struct Cli {
     port: u16,
 }
 
-fn main() {
-    let cli = Cli::parse();
-    println!("{:?}", cli);
+#[tokio::main]
+async fn main() {
+    // let cli = Cli::parse();
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
