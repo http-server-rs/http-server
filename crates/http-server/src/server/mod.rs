a236e26602264cc8ded9289a0bf1c7bb806602e9
Felix Manuel Figueroa <felixmanuelfigueroa@gmail.com>
2023-05-18 20:47:07 -0300

    feat: setup logging with tracing (#291)

--- crates/http-server/src/server/mod.rs
+++ crates/http-server/src/server/mod.rs
@@ -1,8 +1,9 @@
-use axum::Router;
-
 use crate::cli::Cli;
+use axum::{routing::get, Router};
+use tracing::{info, Level};
+use tracing_subscriber::EnvFilter;
 
-pub struct Server(Router);
+pub struct Server(pub Router);
 
 impl Server {
     pub fn router(self) -> Router {
@@ -12,12 +13,16 @@ impl Server {
 
 impl From<Cli> for Server {
     fn from(value: Cli) -> Self {
+        let filter = EnvFilter::from_default_env().add_directive(Level::INFO.into());
+
+        tracing_subscriber::fmt().with_env_filter(filter).init();
+
         let mut app = Router::new();
 
         if value.port == 7878 {
-            app = app.route("/", axum::routing::get(|| async { "Hello, Rust!" }));
+            app = app.route("/", get(|| async { info!("Hello, Rust!") }));
         } else {
-            app = app.route("/", axum::routing::get(|| async { "Hello, World!" }));
+            app = app.route("/", get(|| async { info!("Hello, World!") }));
         }
 
         Self(app)
