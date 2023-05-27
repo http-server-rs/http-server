77016418b2a231874762ca55550d0904e30896c1
Esteban Borai <estebanborai@gmail.com>
2023-05-27 17:21:18 -0400

    feat: provide fs serving support

---
+++ crates/file-explorer/src/fs.rs
@@ -0,0 +1,140 @@
+//! File System utilities for the file explorer
+
+use std::fs::Metadata;
+use std::mem::MaybeUninit;
+use std::path::PathBuf;
+use std::pin::Pin;
+use std::task::{self, Poll};
+
+use axum::body::Bytes;
+use chrono::{DateTime, Local};
+use futures::Stream;
+use mime_guess::{from_path, Mime};
+use tokio::fs::OpenOptions;
+use tokio::io::{AsyncRead, ReadBuf};
+
+use crate::Result;
+
+pub const FILE_BUFFER_SIZE: usize = 8 * 1024;
+
+pub type FileBuffer = Box<[MaybeUninit<u8>; FILE_BUFFER_SIZE]>;
+
+/// Wrapper around `tokio::fs::File` providing `std::fs::Metadata` and the
+/// path to the file
+#[derive(Debug)]
+pub struct File {
+    pub path: PathBuf,
+    pub file: tokio::fs::File,
+    pub metadata: Metadata,
+}
+
+impl File {
+    pub fn new(path: PathBuf, file: tokio::fs::File, metadata: Metadata) -> Self {
+        File {
+            path,
+            file,
+            metadata,
+        }
+    }
+
+    pub fn mime(&self) -> Mime {
+        from_path(self.path.clone()).first_or_octet_stream()
+    }
+
+    pub fn size(&self) -> u64 {
+        self.metadata.len()
+    }
+
+    pub fn last_modified(&self) -> Result<DateTime<Local>> {
+        let modified = self.metadata.modified()?;
+        let modified: DateTime<Local> = modified.into();
+
+        Ok(modified)
+    }
+
+    #[allow(dead_code)]
+    pub fn bytes(self) -> Vec<u8> {
+        let byte_stream = ByteStream {
+            file: self.file,
+            buffer: Box::new([MaybeUninit::uninit(); FILE_BUFFER_SIZE]),
+        };
+
+        byte_stream
+            .buffer
+            .iter()
+            .map(|muint| unsafe { muint.assume_init() })
+            .collect::<Vec<u8>>()
+    }
+}
+
+pub struct ByteStream {
+    file: tokio::fs::File,
+    buffer: FileBuffer,
+}
+
+impl From<File> for ByteStream {
+    fn from(file: File) -> Self {
+        ByteStream {
+            file: file.file,
+            buffer: Box::new([MaybeUninit::uninit(); FILE_BUFFER_SIZE]),
+        }
+    }
+}
+
+impl Stream for ByteStream {
+    type Item = Result<Bytes>;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut task::Context<'_>) -> Poll<Option<Self::Item>> {
+        let ByteStream {
+            ref mut file,
+            ref mut buffer,
+        } = *self;
+        let mut read_buffer = ReadBuf::uninit(&mut buffer[..]);
+
+        match Pin::new(file).poll_read(cx, &mut read_buffer) {
+            Poll::Ready(Ok(())) => {
+                let filled = read_buffer.filled();
+
+                if filled.is_empty() {
+                    Poll::Ready(None)
+                } else {
+                    Poll::Ready(Some(Ok(Bytes::copy_from_slice(filled))))
+                }
+            }
+            Poll::Ready(Err(error)) => Poll::Ready(Some(Err(error.into()))),
+            Poll::Pending => Poll::Pending,
+        }
+    }
+}
+
+/// Representation of a directory
+#[derive(Debug)]
+pub struct Directory {
+    path: PathBuf,
+}
+
+/// An entry in a directory
+#[derive(Debug)]
+pub enum Entry {
+    File(Box<File>),
+    Directory(Directory),
+}
+
+#[cfg(not(target_os = "windows"))]
+pub async fn open(path: PathBuf) -> Result<Entry> {
+    let mut open_options = OpenOptions::new();
+    let entry_path: PathBuf = path.clone();
+    let file = open_options.read(true).open(path).await?;
+    let metadata = file.metadata().await?;
+
+    if metadata.is_dir() {
+        return Ok(Entry::Directory(Directory { path: entry_path }));
+    }
+
+    Ok(Entry::File(Box::new(File::new(entry_path, file, metadata))))
+}
+
+#[cfg(target_os = "windows")]
+async fn open(path: PathBuf) -> Result<Entry> {
+    todo!("Windows support is not yet implemented")
+}
--- crates/file-explorer/src/lib.rs
+++ crates/file-explorer/src/lib.rs
@@ -1,62 +1,110 @@
+mod fs;
+mod http;
+
 use std::convert::Infallible;
-use std::future::Future;
 use std::path::PathBuf;
-use std::pin::Pin;
-use std::task::{Context, Poll};
+use std::task::Poll;
 
 use axum::body::Body;
 use axum::http::{Request, StatusCode};
 use axum::response::Response;
+use futures::future::BoxFuture;
+use thiserror::Error;
 use tower::Service;
 
-#[derive(Debug)]
-pub struct FileExplorerResponseFuture {
-    status_code: StatusCode,
-    uri: String,
+use self::fs::open;
+use self::http::{make_http_file_response, CacheControlDirective};
+
+#[derive(Debug, Error)]
+pub enum FileExplorerError {
+    #[error("Failed to open file")]
+    OpenFile(#[from] std::io::Error),
 }
 
-impl Future for FileExplorerResponseFuture {
-    type Output = Result<Response<Body>, Infallible>;
+pub type Result<T> = std::result::Result<T, FileExplorerError>;
 
-    fn poll(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let mut res = Response::default();
-
-        *res.status_mut() = self.status_code;
-        *res.body_mut() = Body::from(self.uri.clone());
-
-        Poll::Ready(Ok(res))
-    }
-}
+pub type FileExplorerResponse = Response<Body>;
 
 #[derive(Clone)]
 pub struct FileExplorer {
-    root_dir: PathBuf,
+    base_dir: PathBuf,
 }
 
 impl FileExplorer {
-    pub fn new(root_dir: PathBuf) -> Self {
-        Self { root_dir }
+    pub fn new(base_dir: PathBuf) -> Self {
+        Self { base_dir }
+    }
+
+    /// Create an absolute path from the base directory and the given path.
+    fn absolute_path(&self, path: &str) -> PathBuf {
+        let path = path.trim().replace("../", "");
+        let mut absolute_path = self.base_dir.clone();
+
+        // If the path starts with a slash, it's already absolute.
+        if path == "/" || path.is_empty() {
+            return absolute_path;
+        }
+
+        absolute_path.push(path);
+        absolute_path
     }
 }
 
 impl Service<Request<Body>> for FileExplorer {
-    type Response = Response<Body>;
+    type Response = FileExplorerResponse;
     type Error = Infallible;
-    type Future = FileExplorerResponseFuture;
+    type Future = BoxFuture<'static, std::result::Result<Self::Response, Self::Error>>;
 
     fn poll_ready(
         &mut self,
         _cx: &mut std::task::Context<'_>,
-    ) -> std::task::Poll<Result<(), Self::Error>> {
+    ) -> std::task::Poll<std::result::Result<(), Self::Error>> {
         Poll::Ready(Ok(()))
     }
 
     fn call(&mut self, req: Request<Body>) -> Self::Future {
-        tracing::info!("Received request! Attempt to find on {:?}", self.root_dir);
+        let path = req.uri().path();
+        let entry_path = self.absolute_path(path);
 
-        FileExplorerResponseFuture {
-            status_code: StatusCode::OK,
-            uri: req.uri().to_string(),
+        Box::pin(async move {
+            let entry = open(entry_path).await.unwrap();
+            let response = match entry {
+                fs::Entry::File(file) => {
+                    let response =
+                        make_http_file_response(*file, CacheControlDirective::NoCache).await;
+
+                    response.unwrap()
+                }
+                fs::Entry::Directory(_) => Response::builder()
+                    .status(StatusCode::NOT_FOUND)
+                    .body(Body::empty())
+                    .unwrap(),
+            };
+
+            Ok(response)
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn makes_absolute_path() {
+        let explorer = FileExplorer::new(PathBuf::from("/Users/John/Destkop/www"));
+        let cases = vec![
+            ("", "/Users/John/Destkop/www"),
+            ("/", "/Users/John/Destkop/www"),
+            ("/ ", "/Users/John/Destkop/www"),
+            ("foo", "/Users/John/Destkop/www/foo"),
+            ("foo/bar", "/Users/John/Destkop/www/foo/bar"),
+            ("foo/bar/", "/Users/John/Destkop/www/foo/bar/"),
+            ("../../foo/", "/Users/John/Destkop/www/foo/"),
+        ];
+
+        for (rel_path, full_path) in cases {
+            assert_eq!(explorer.absolute_path(rel_path), PathBuf::from(full_path));
         }
     }
 }
