1168e0353d01b07232628410df91b1314ea4150b
Esteban Borai <estebanborai@gmail.com>
2024-10-12 20:56:39 -0300

    feat: introduce templater

--- crates/file-explorer/src/lib.rs
+++ crates/file-explorer/src/lib.rs
@@ -1,10 +1,16 @@
+mod templater;
+
+use std::path::PathBuf;
 use std::sync::Arc;
 
+use anyhow::Result;
 use http_body_util::Full;
 use hyper::body::{Bytes, Incoming};
-use hyper::{Request, Response};
+use hyper::{Method, Request, Response};
 
 use http_server_plugin::{export_plugin, Function, InvocationError, PluginRegistrar};
+
+use self::templater::Templater;
 
 export_plugin!(register);
 
@@ -12,18 +18,32 @@ export_plugin!(register);
 extern "C" fn register(registrar: &mut dyn PluginRegistrar) {
     registrar.register_function(
         "file-explorer",
-        Arc::new(FileExplorer {
-            path: String::from("/"),
-        }),
+        Arc::new(FileExplorer::new(PathBuf::new()).expect("Failed to create FileExplorer")),
     );
 }
 
 pub struct FileExplorer {
-    pub path: String,
+    pub path: PathBuf,
+    pub templater: Templater,
 }
 
 impl Function for FileExplorer {
-    fn call(&self, _: Request<Incoming>) -> Result<Response<Full<Bytes>>, InvocationError> {
-        Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+    fn call(&self, req: Request<Incoming>) -> Result<Response<Full<Bytes>>, InvocationError> {
+        match req.method() {
+            &Method::GET => Ok(Response::new(Full::new(Bytes::from("File Explorer")))),
+            &Method::POST => Ok(Response::new(Full::new(Bytes::from("Prepare to upload")))),
+            _ => Ok(Response::new(Full::new(Bytes::from("Unsupported method")))),
+        }
+    }
+}
+
+impl FileExplorer {
+    pub fn new(path: PathBuf) -> Result<Self> {
+        let templater = Templater::new()?;
+
+        Ok(Self {
+            path,
+            templater,
+        })
     }
 }
