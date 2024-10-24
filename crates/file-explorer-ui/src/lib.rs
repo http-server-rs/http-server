680828049c16ec41d2e342bb9e868d88ba0b9c88
Esteban Borai <estebanborai@gmail.com>
2024-10-24 19:48:50 -0300

    feat: serve ui files

--- crates/file-explorer-ui/src/lib.rs
+++ crates/file-explorer-ui/src/lib.rs
@@ -1,9 +1,15 @@
-use leptos::{component, view, IntoView};
+use leptos::{component, spawn_local, view, IntoView};
 use leptos_meta::provide_meta_context;
+use rust_embed::Embed;
 
 #[component]
 pub fn App() -> impl IntoView {
     provide_meta_context();
+
+    spawn_local(async move {
+        leptos::logging::warn!("Performing a request to the server");
+        reqwest::get("http://localhost:3000/api/v1").await.unwrap();
+    });
 
     view! {
         <div>
@@ -11,3 +17,7 @@ pub fn App() -> impl IntoView {
         </div>
     }
 }
+
+#[derive(Embed)]
+#[folder = "public/dist"]
+pub struct Assets;
