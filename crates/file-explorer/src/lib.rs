1bfc1e659c67d34c41ac634b3f40d591f65f8555
Esteban Borai <estebanborai@gmail.com>
2025-01-04 17:59:16 -0300

    chore: rename crates (#453)
    Rename File Explorer Crates to be:
    
    - `file-explorer`: Crate for File Explorer Logic, agnostic to UI/Plugin
    - `file-explorer-plugin`: HTTP Server RS Plugin
    
    <!--
    Developer's Certificate of Origin 1.1
    
    By making a contribution to this project, I certify that:
    
    (a) The contribution was created in whole or in part by me and I
        have the right to submit it under the open source license
        indicated in the file; or
    
    (b) The contribution is based upon previous work that, to the best
        of my knowledge, is covered under an appropriate open source
        license and I have the right under that license to submit that
        work with modifications, whether created in whole or in part
        by me, under the same open source license (unless I am
        permitted to submit under a different license), as indicated
        in the file; or
    
    (c) The contribution was provided directly to me by some other
        person who certified (a), (b) or (c) and I have not modified
        it.
    
    (d) I understand and agree that this project and the contribution
        are public and that a record of the contribution (including all
        personal information I submit with it, including my sign-off) is
        maintained indefinitely and may be redistributed consistent with
        this project or the open source license(s) involved.
    -->

--- crates/file-explorer/src/lib.rs
+++ crates/file-explorer/src/lib.rs
@@ -1,387 +1,113 @@
-mod utils;
+mod fs;
 
-use std::fs::read_dir;
-use std::mem::MaybeUninit;
-use std::path::{Component, Path, PathBuf};
-use std::sync::Arc;
+use std::path::{Component, PathBuf};
 
-use anyhow::{Context, Result};
-use async_trait::async_trait;
-use http::request::Parts;
-use http::HeaderValue;
-use http_body_util::Full;
-use hyper::body::Bytes;
-use hyper::header::CONTENT_TYPE;
-use hyper::{Method, Response, StatusCode, Uri};
-use multer::Multipart;
-use percent_encoding::{percent_decode_str, utf8_percent_encode};
-use serde::Deserialize;
-use tokio::io::AsyncWriteExt;
-use tokio::runtime::Handle;
+use tokio::fs::OpenOptions;
 
-use file_explorer_core::{Entry, FileExplorer};
-use file_explorer_proto::{BreadcrumbItem, DirectoryEntry, DirectoryIndex, EntryType, Sort};
-use file_explorer_ui::Assets;
-use http_server_plugin::config::read_from_path;
-use http_server_plugin::{export_plugin, Function, InvocationError, PluginRegistrar};
+pub use self::fs::{Directory, File};
 
-use self::utils::{decode_uri, encode_uri, PERCENT_ENCODE_SET};
+use anyhow::Result;
 
-const FILE_BUFFER_SIZE: usize = 8 * 1024;
-
-pub type FileBuffer = Box<[MaybeUninit<u8>; FILE_BUFFER_SIZE]>;
-
-export_plugin!(register);
-
-const PLUGIN_NAME: &str = "file-explorer";
-
-#[allow(improper_ctypes_definitions)]
-extern "C" fn register(config_path: PathBuf, rt: Arc<Handle>, registrar: &mut dyn PluginRegistrar) {
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::INFO)
-        .init();
-
-    let config: FileExplorerConfig = read_from_path(config_path, PLUGIN_NAME).unwrap();
-
-    registrar.register_function(
-        PLUGIN_NAME,
-        Arc::new(FileExplorerPlugin::new(rt, config.path)),
-    );
+/// Any OS filesystem entry recognized by [`FileExplorer`] is treated as a
+/// `Entry` both `File` and `Directory` are possible values.
+#[derive(Debug)]
+pub enum Entry {
+    File(Box<File>),
+    Directory(Directory),
 }
 
-#[derive(Debug, Deserialize)]
-struct FileExplorerConfig {
-    pub path: PathBuf,
+pub struct FileExplorer {
+    root: PathBuf,
 }
 
-struct FileExplorerPlugin {
-    file_explorer: FileExplorer,
-    path: PathBuf,
-    rt: Arc<Handle>,
-}
-
-#[async_trait]
-impl Function for FileExplorerPlugin {
-    async fn call(
-        &self,
-        parts: Parts,
-        body: Bytes,
-    ) -> Result<Response<Full<Bytes>>, InvocationError> {
-        self.rt
-            .block_on(async move { self.handle(parts, body).await })
-    }
-}
-
-impl FileExplorerPlugin {
-    fn new(rt: Arc<Handle>, path: PathBuf) -> Self {
-        let file_explorer = FileExplorer::new(path.clone());
-
-        Self {
-            file_explorer,
-            path,
-            rt,
-        }
+impl FileExplorer {
+    pub fn new(root: PathBuf) -> Self {
+        Self { root }
     }
 
-    async fn handle(
-        &self,
-        parts: Parts,
-        body: Bytes,
-    ) -> Result<Response<Full<Bytes>>, InvocationError> {
-        tracing::info!("Handling request: {:?}", parts);
-
-        if parts.uri.path().starts_with("/api/v1") {
-            self.handle_api(parts, body).await
-        } else {
-            let path = parts.uri.path();
-            let path = path.strip_prefix('/').unwrap_or(path);
-
-            if let Some(file) = Assets::get(path) {
-                let content_type = mime_guess::from_path(path).first_or_octet_stream();
-                let content_type = HeaderValue::from_str(content_type.as_ref()).unwrap();
-                let body = Full::new(Bytes::from(file.data.to_vec()));
-                let mut response = Response::new(body);
-                let mut headers = response.headers().clone();
-
-                headers.append(CONTENT_TYPE, content_type);
-                *response.headers_mut() = headers;
-
-                return Ok(response);
-            }
-
-            let index = Assets::get("index.html").unwrap();
-            let body = Full::new(Bytes::from(index.data.to_vec()));
-            let mut response = Response::new(body);
-            let mut headers = response.headers().clone();
-
-            headers.append(CONTENT_TYPE, "text/html".try_into().unwrap());
-            *response.headers_mut() = headers;
-
-            Ok(response)
-        }
+    /// Peeks on the provided `path` as a "subpath" for this [`FileExplorer`] instance.
+    pub async fn peek(&self, path: PathBuf) -> Result<Entry> {
+        let relative_path = self.build_relative_path(path);
+        self.open(relative_path).await
     }
 
-    async fn handle_api(
-        &self,
-        parts: Parts,
-        body: Bytes,
-    ) -> Result<Response<Full<Bytes>>, InvocationError> {
-        let path = Self::parse_req_uri(parts.uri.clone()).unwrap();
+    /// Joins the provided `path` with the `root` path of this [`FileExplorer`] instance.
+    fn build_relative_path(&self, path: PathBuf) -> PathBuf {
+        let mut root = self.root.clone();
+        root.extend(&self.normalize_path(&path));
+        root
+    }
 
-        match parts.method {
-            Method::GET => match self.file_explorer.peek(path).await {
-                Ok(entry) => match entry {
-                    Entry::Directory(dir) => {
-                        let directory_index =
-                            self.marshall_directory_index(dir.path()).await.unwrap();
-                        let json = serde_json::to_string(&directory_index).unwrap();
-                        let body = Full::new(Bytes::from(json));
-                        let mut response = Response::new(body);
-                        let mut headers = response.headers().clone();
-
-                        headers.append(CONTENT_TYPE, "application/json".try_into().unwrap());
-                        *response.headers_mut() = headers;
-
-                        Ok(response)
-                    }
-                    Entry::File(mut file) => {
-                        let body = Full::new(Bytes::from(file.bytes().await.unwrap()));
-                        let mut response = Response::new(body);
-                        let mut headers = response.headers().clone();
-
-                        headers.append(CONTENT_TYPE, file.mime().to_string().try_into().unwrap());
-                        *response.headers_mut() = headers;
-
-                        Ok(response)
-                    }
-                },
-                Err(err) => {
-                    let message = format!("Failed to resolve path: {}", err);
-                    Ok(Response::new(Full::new(Bytes::from(message))))
+    /// Normalizes a `Path` to be directory-traversal safe.
+    ///
+    /// ```ignore
+    /// docs/collegue/cs50/lectures/../code/voting_excecise
+    /// ```
+    ///
+    /// Will be normalized to be:
+    ///
+    /// ```ignore
+    /// docs/collegue/cs50/code/voting_excecise
+    /// ```
+    ///
+    /// # Reference
+    ///
+    /// - https://owasp.org/www-community/attacks/Path_Traversal
+    fn normalize_path(&self, path: &PathBuf) -> PathBuf {
+        path.components()
+            .fold(PathBuf::new(), |mut result, p| match p {
+                Component::ParentDir => {
+                    result.pop();
+                    result
                 }
-            },
-            Method::POST => {
-                self.handle_file_upload(parts, body).await?;
-                Ok(Response::new(Full::new(Bytes::from(
-                    "POST method is not supported",
-                ))))
-            }
-            _ => Ok(Response::new(Full::new(Bytes::from("Unsupported method")))),
-        }
-    }
-
-    async fn handle_file_upload(
-        &self,
-        parts: Parts,
-        body: Bytes,
-    ) -> Result<Response<Full<Bytes>>, InvocationError> {
-        // Extract the `multipart/form-data` boundary from the headers.
-        let boundary = parts
-            .headers
-            .get(CONTENT_TYPE)
-            .and_then(|ct| ct.to_str().ok())
-            .and_then(|ct| multer::parse_boundary(ct).ok());
-
-        // Send `BAD_REQUEST` status if the content-type is not multipart/form-data.
-        if boundary.is_none() {
-            return Ok(Response::builder()
-                .status(StatusCode::BAD_REQUEST)
-                .body(Full::from("BAD REQUEST"))
-                .unwrap());
-        }
-
-        // Process the multipart e.g. you can store them in files.
-        if let Err(err) = self.process_multipart(body, boundary.unwrap()).await {
-            return Ok(Response::builder()
-                .status(StatusCode::INTERNAL_SERVER_ERROR)
-                .body(Full::from(format!("INTERNAL SERVER ERROR: {}", err)))
-                .unwrap());
-        }
-
-        Ok(Response::new(Full::from("Success")))
-    }
-
-    async fn process_multipart(&self, bytes: Bytes, boundary: String) -> multer::Result<()> {
-        let cursor = std::io::Cursor::new(bytes);
-        let bytes_stream = tokio_util::io::ReaderStream::new(cursor);
-        let mut multipart = Multipart::new(bytes_stream, boundary);
-
-        // Iterate over the fields, `next_field` method will return the next field if
-        // available.
-        while let Some(mut field) = multipart.next_field().await? {
-            // Get the field name.
-            let name = field.name();
-
-            // Get the field's filename if provided in "Content-Disposition" header.
-            let file_name = field.file_name().to_owned().unwrap_or("default.png");
-
-            // Get the "Content-Type" header as `mime::Mime` type.
-            let content_type = field.content_type();
-
-            let mut file = tokio::fs::File::create(file_name).await.unwrap();
-
-            println!(
-                "\n\nName: {:?}, FileName: {:?}, Content-Type: {:?}\n\n",
-                name, file_name, content_type
-            );
-
-            // Process the field data chunks e.g. store them in a file.
-            let mut field_bytes_len = 0;
-            while let Some(field_chunk) = field.chunk().await? {
-                // Do something with field chunk.
-                field_bytes_len += field_chunk.len();
-                file.write_all(&field_chunk).await.unwrap();
-            }
-
-            println!("Field Bytes Length: {:?}", field_bytes_len);
-        }
-
-        Ok(())
-    }
-
-    fn parse_req_uri(uri: Uri) -> Result<PathBuf> {
-        let parts: Vec<&str> = uri.path().split('/').collect();
-        let path = &parts[3..].join("/");
-
-        Ok(decode_uri(path))
-    }
-
-    /// Encodes a `PathBuf` component using `PercentEncode` with UTF-8 charset.
-    ///
-    /// # Panics
-    ///
-    /// If the component's `OsStr` representation doesn't belong to valid UTF-8
-    /// this function panics.
-    fn encode_component(comp: Component) -> String {
-        let component = comp
-            .as_os_str()
-            .to_str()
-            .expect("The provided OsStr doesn't belong to the UTF-8 charset.");
-
-        utf8_percent_encode(component, PERCENT_ENCODE_SET).to_string()
-    }
-
-    fn breadcrumbs_from_path(root_dir: &Path, path: &Path) -> Result<Vec<BreadcrumbItem>> {
-        let root_dir_name = root_dir
-            .components()
-            .last()
-            .unwrap()
-            .as_os_str()
-            .to_str()
-            .expect("The first path component is not UTF-8 charset compliant.");
-        let stripped = path
-            .strip_prefix(root_dir)?
-            .components()
-            .map(Self::encode_component)
-            .collect::<Vec<String>>();
-
-        let mut breadcrumbs = stripped
-            .iter()
-            .enumerate()
-            .map(|(idx, entry_name)| BreadcrumbItem {
-                depth: (idx + 1) as u8,
-                entry_name: percent_decode_str(entry_name)
-                    .decode_utf8()
-                    .expect("The path name is not UTF-8 compliant")
-                    .to_string(),
-                entry_link: format!("/{}", stripped[0..=idx].join("/")),
+                Component::Normal(os_string) => {
+                    result.push(os_string);
+                    result
+                }
+                _ => result,
             })
-            .collect::<Vec<BreadcrumbItem>>();
-
-        breadcrumbs.insert(
-            0,
-            BreadcrumbItem {
-                depth: 0,
-                entry_name: String::from(root_dir_name),
-                entry_link: String::from("/"),
-            },
-        );
-
-        Ok(breadcrumbs)
     }
 
-    /// Creates entry's relative path. Used by Handlebars template engine to
-    /// provide navigation through `FileExplorer`
-    ///
-    /// If the root_dir is: `https-server/src`
-    /// The entry path is: `https-server/src/server/service/file_explorer.rs`
-    ///
-    /// Then the resulting path from this function is the absolute path to
-    /// the "entry path" in relation to the "root_dir" path.
-    ///
-    /// This happens because links should behave relative to the `/` path
-    /// which in this case is `http-server/src` instead of system's root path.
-    fn make_dir_entry_link(root_dir: &Path, entry_path: &Path) -> String {
-        let path = entry_path.strip_prefix(root_dir).unwrap();
+    #[cfg(not(target_os = "windows"))]
+    async fn open(&self, path: PathBuf) -> Result<Entry> {
+        let mut open_options = OpenOptions::new();
+        let entry_path: PathBuf = path.clone();
+        let file = open_options.read(true).open(path).await?;
+        let metadata = file.metadata().await?;
 
-        encode_uri(path)
-    }
-
-    /// Creates a `DirectoryIndex` with the provided `root_dir` and `path`
-    /// (HTTP Request URI)
-    fn index_directory(root_dir: PathBuf, path: PathBuf) -> Result<DirectoryIndex> {
-        let breadcrumbs = Self::breadcrumbs_from_path(&root_dir, &path)?;
-        let entries = read_dir(path).context("Unable to read directory")?;
-        let mut directory_entries: Vec<DirectoryEntry> = Vec::new();
-
-        for entry in entries {
-            let entry = entry.context("Unable to read entry")?;
-            let metadata = entry.metadata()?;
-
-            let display_name = entry
-                .file_name()
-                .to_str()
-                .context("Unable to gather file name into a String")?
-                .to_string();
-
-            let date_created = if let Ok(time) = metadata.created() {
-                Some(time.into())
-            } else {
-                None
-            };
-
-            let date_modified = if let Ok(time) = metadata.modified() {
-                Some(time.into())
-            } else {
-                None
-            };
-
-            let entry_type = if metadata.file_type().is_dir() {
-                EntryType::Directory
-            } else if let Some(ext) = display_name.split(".").last() {
-                match ext.to_ascii_lowercase().as_str() {
-                    "gitignore" | "gitkeep" => EntryType::Git,
-                    "justfile" => EntryType::Justfile,
-                    "md" => EntryType::Markdown,
-                    "rs" => EntryType::Rust,
-                    "toml" => EntryType::Toml,
-                    _ => EntryType::File,
-                }
-            } else {
-                EntryType::File
-            };
-
-            directory_entries.push(DirectoryEntry {
-                is_dir: metadata.is_dir(),
-                size_bytes: metadata.len(),
-                entry_path: Self::make_dir_entry_link(&root_dir, &entry.path()),
-                display_name,
-                entry_type,
-                date_created,
-                date_modified,
-            });
+        if metadata.is_dir() {
+            return Ok(Entry::Directory(Directory { path: entry_path }));
         }
 
-        directory_entries.sort();
-
-        Ok(DirectoryIndex {
-            entries: directory_entries,
-            breadcrumbs,
-            sort: Sort::Directory,
-        })
+        Ok(Entry::File(Box::new(File::new(entry_path, file, metadata))))
     }
 
-    async fn marshall_directory_index(&self, path: PathBuf) -> Result<DirectoryIndex> {
-        Self::index_directory(self.path.clone(), path)
+    #[cfg(target_os = "windows")]
+    async fn open(&self, path: PathBuf) -> Result<Entry> {
+        /// The file is being opened or created for a backup or restore operation.
+        /// The system ensures that the calling process overrides file security
+        /// checks when the process has SE_BACKUP_NAME and SE_RESTORE_NAME privileges.
+        ///
+        /// For more information, see Changing Privileges in a Token.
+        /// You must set this flag to obtain a handle to a directory.
+        /// A directory handle can be passed to some functions instead of a file handle.
+        ///
+        /// Refer: https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-createfilea
+        const FILE_FLAG_BACKUP_SEMANTICS: u32 = 0x02000000;
+
+        let mut open_options = OpenOptions::new();
+        let entry_path: PathBuf = path.clone();
+        let file = open_options
+            .read(true)
+            .custom_flags(FILE_FLAG_BACKUP_SEMANTICS)
+            .open(path)
+            .await?;
+        let metadata = file.metadata().await?;
+
+        if metadata.is_dir() {
+            return Ok(Entry::Directory(Directory { path: entry_path }));
+        }
+
+        Ok(Entry::File(Box::new(File::new(entry_path, file, metadata))))
     }
 }
